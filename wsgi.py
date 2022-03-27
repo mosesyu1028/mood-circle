dbe695ddaa81f3d7d909e21f6c2ea31e8d8700d2
yu_0901 <mosesyu1028@gmail.com>
2022-03-27 10:56:24 +0800

    friendship ended with python

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-from project import create_app
-# from waitress import serve
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
-    # serve(app, host='0.0.0.0', port=8080)
